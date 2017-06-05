f9bd61620d5541263c3df3460694f79e77b4034c
Hoang Nguyen <hoang.nguyen@appscore.com.au>
2017-06-05 16:11:58 +1000

    Use translator adapter

---
+++ src/HND/SymfonyForm/Translator/LaravelTranslatorAdapter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace HND\SymfonyForm\Translator;
+
+use Illuminate\Contracts\Translation\Translator;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class LaravelTranslatorAdapter implements TranslatorInterface
+{
+    protected $translator;
+
+    /**
+     * LaravelTranslatorAdapter constructor.
+     * @param Translator $translator
+     */
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function trans($id, array $parameters = array(), $domain = null, $locale = null){
+        $this->translator->trans($id, $parameters, $locale);
+    }
+
+    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null){
+        $this->translator->transChoice($id, $number, $parameters, $locale);
+    }
+
+    public function setLocale($locale){
+        $this->translator->setLocale($locale);
+    }
+
+    public function getLocale(){
+        return $this->translator->getLocale();
+    }
+}
